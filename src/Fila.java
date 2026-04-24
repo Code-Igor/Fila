6c3084ebce55a672f2458e834954bb1f7b5a5282
Igor <igordossantoslopes73@gmail.com>
2026-04-24 20:00:21 -0300

    First commit

---
+++ src/Fila.java
@@ -0,0 +1,27 @@
+public class Fila {
+
+    private Integer[] vetor;
+
+    public Fila (int tamanho) {
+        this.vetor = new Integer[tamanho];
+    }
+
+    /// funcionalidades:
+    /// Inserir no final (enfileirar)
+    /// Remover do início (desenfileirar)
+    /// Verificar se a fila está vazia
+
+    public void enfileirar(int valor) {
+
+    }
+
+    public Integer desenfileirar() {
+        Integer valor = 0;
+        return valor;
+    }
+
+    public boolean filaVazia() {
+
+        return false;
+    }
+}
