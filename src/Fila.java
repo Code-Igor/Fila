f8f46934192e4dc35b2742b0e2f920f72a2a001e
Igor <igordossantoslopes73@gmail.com>
2026-04-24 22:44:07 -0300

    fila implementada com as 3 funcionalidades

--- src/Fila.java
+++ src/Fila.java
@@ -1,30 +1,42 @@
 public class Fila {
 
     private Integer[] vetor;
-    private int inicio;
-    private int fim;
     private int tamanho;
 
-    public Fila (int tamanho) {
-        this.vetor = new Integer[tamanho];
+    public Fila(int capacidade) {
+        this.vetor = new Integer[capacidade];
+        this.tamanho = 0;
     }
 
-    /// funcionalidades:
-    /// Inserir no final (enfileirar)
-    /// Remover do início (desenfileirar)
-    /// Verificar se a fila está vazia
-
     public void enfileirar(int valor) {
-
+        if (tamanho >= vetor.length) {
+            throw new IllegalStateException("fila cheia!");
+        }
+        // insere no final da fila
+        vetor[tamanho] = valor;
+        tamanho++;
     }
 
     public Integer desenfileirar() {
-        Integer valor = 0;
+        if (filaVazia()) {
+            throw new IllegalStateException("Fila vazia!");
+        }
+        Integer valor = vetor[0];
+
+        // move todos os elementos uma posicao para a esquerda
+        // para manter o inicio sempre no indice 0
+        for (int i = 1; i < tamanho; i++) {
+            vetor[i - 1] = vetor[i];
+        }
+
+        vetor[tamanho - 1] = null; //
+        tamanho--;
+
         return valor;
     }
 
     public boolean filaVazia() {
-
-        return false;
+        return tamanho == 0;
     }
 }
+
